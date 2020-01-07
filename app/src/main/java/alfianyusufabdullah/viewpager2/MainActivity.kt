26f802bdf9375037d028f7430422fe7bd76e5695
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-01-07 14:48:24 +0700

    implement viewpager2

--- app/src/main/java/alfianyusufabdullah/viewpager2/MainActivity.kt
+++ app/src/main/java/alfianyusufabdullah/viewpager2/MainActivity.kt
@@ -2,11 +2,25 @@ package alfianyusufabdullah.viewpager2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val texts = listOf("Page 1", "Page 2", "Page 3")
+
+        val adapter = MainAdapter(texts) {
+            if (mainViewPager.orientation == ViewPager2.ORIENTATION_VERTICAL) {
+                mainViewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            } else {
+                mainViewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
+            }
+        }
+
+        mainViewPager.adapter = adapter
     }
 }
